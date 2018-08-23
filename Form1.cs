8ebea71d923ca67a00415b92e17322f46167776a
foxfire <foxfire256@gmail.com>
2018-08-23 18:49:42 -0400

    This should be working now

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,17 @@ namespace LoanCalcCsForms
 		{
 			InitializeComponent();
 		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			//M = P * ( J / (1 - (1 + J)^-N))
+			double J = Double.Parse(textBox2.Text);
+			J = J / 100.0 / 12.0;
+			double P = Double.Parse(textBox1.Text);
+			double N = Double.Parse(textBox3.Text);
+			double M = P * (J / (1 - Math.Pow(1 + J, -N)));
+			M = Math.Round(M, 2);
+			textBox4.Text = M.ToString();
+		}
 	}
 }
